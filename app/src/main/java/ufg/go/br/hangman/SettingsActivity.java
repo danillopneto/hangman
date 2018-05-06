53146084af8559953252f3ba7b0930e9c210d45e
viniciustads <vinicius_venancio2007@hotmail.com>
2018-05-05 23:12:16 -0300

    SharedPreferences com switch
    Salvando em memoria as configurações dos botões

--- app/src/main/java/ufg/go/br/hangman/SettingsActivity.java
+++ app/src/main/java/ufg/go/br/hangman/SettingsActivity.java
@@ -1,17 +1,13 @@
 package ufg.go.br.hangman;
-
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
 import android.widget.Switch;
-
 import ufg.go.br.hangman.Util.SoundGame;
 import ufg.go.br.hangman.Util.VibrationGame;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
 
     Switch switchsound;
     Switch switchvibrate;
@@ -24,89 +20,90 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-
         switchsound = (Switch) findViewById(R.id.switchsound);
         switchvibrate = (Switch) findViewById(R.id.switchvibrate);
         switchreveal = (Switch) findViewById(R.id.switchreveal);
 
+        // setta os valores do shared preferences, caso não tenha nenhum o valor padrão é falso
+        switchsound.setChecked(getSwitch("switchsound"));
+        switchvibrate.setChecked(getSwitch("switchvibrate"));
+        switchreveal.setChecked(getSwitch("switchreveal"));
 
-        SharedPreferences preferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
-        SharedPreferences.Editor ed  = preferences.edit();
-
-        switchsound.setChecked(preferences.getBoolean("switchsound",true));
-        switchvibrate.setChecked(preferences.getBoolean("switchvibrate",true));
-        switchreveal.setChecked(preferences.getBoolean("switchreveal",true));
-
-
-
+        setMusicOnOff();
+        setVibrateOnOff();
+        setRevealOnOff();
     }
 
-
-    public void setMusicOnOff(View v) {
-
+    public void setMusicOnOff() {
         switchsound = (Switch) findViewById(R.id.switchsound);
-
-        switchsound.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                SharedPreferences preferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
-                SharedPreferences.Editor ed  = preferences.edit();
-
+        switchsound.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(switchsound.isChecked()){
-                    ed.putBoolean("switchsound", true);
+                    saveSwitch("switchsound", true);
                 }
                 else {
-                    ed.putBoolean("switchsound", false);
+                    saveSwitch("switchsound", false);
                 }
             }
         });
 
-
-
     }
 
-    public void setVibrateOnOff(View v) {
-
+    public void setVibrateOnOff() {
         switchvibrate = (Switch) findViewById(R.id.switchvibrate);
-
-        switchvibrate.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                SharedPreferences preferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
-                SharedPreferences.Editor ed  = preferences.edit();
-
+        switchvibrate.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(switchvibrate.isChecked()){
-                    ed.putBoolean("switchvibrate", true);
+                    saveSwitch("switchvibrate", true);
                 }
                 else {
-                    ed.putBoolean("switchvibrate", false);
+                    saveSwitch("switchvibrate", false);
                 }
             }
         });
-
-
-
     }
 
 
-    public void setRevealOnOff(View v) {
-
+    public void setRevealOnOff() {
         switchreveal = (Switch) findViewById(R.id.switchreveal);
-
-        switchreveal.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                SharedPreferences preferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
-                SharedPreferences.Editor ed  = preferences.edit();
-
-                if(switchsound.isChecked()){
-                    ed.putBoolean("switchreveal", true);
+        switchreveal.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(switchreveal.isChecked()){
+                    saveSwitch("switchreveal", true);
                 }
                 else {
-                    ed.putBoolean("switchreveal", false);
+                    saveSwitch("switchreveal", false);
                 }
             }
         });
-
-
-
     }
 
+    private boolean getSwitch(String key){
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("settings", android.content.Context.MODE_PRIVATE);
+        return preferences.getBoolean(key, false);
+    }
+    private void saveSwitch(String key,boolean value){
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("settings", android.content.Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(key, value);
+        editor.commit();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch(buttonView.getId()){
+            case R.id.switchreveal:
+                saveSwitch("switchreveal",isChecked);
+                break;
+            case R.id.switchvibrate:
+                saveSwitch("switchvibrate",isChecked);
+                break;
+            case R.id.switchsound:
+                saveSwitch("switchsound",isChecked);
+                break;
+        }
+    }
 }
